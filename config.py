c825988a3fcbe031ef582d86a03c60f71acaf24b
whizakbhai <130620418+whizakbhai@users.noreply.github.com>
2023-11-09 17:39:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 API_ID = API_ID = 90864300
 API_HASH = os.environ.get("API_HASH", "50cf66c0625d75b0d2ed21ee68196370")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6287684948:AAFl1ZfAC6McttYkYwYXNf277zu0VrazEDM")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6398384523:AAGJSQbWw4XaCgF5fw_PlX_LKVHklvLM9WQ")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
